96efd3841b8a3e76d71fad56001f8e61154e5909
PeterGamerrr <peterrr@assenbergh.nl>
2026-01-23 14:31:25 +0100

    made printer minigame a world object

--- OfficePirate/Assets/Game/Scripts/DayController.cs
+++ OfficePirate/Assets/Game/Scripts/DayController.cs
@@ -5,39 +5,39 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class DayController : MonoBehaviour
-{
-    
-    [SerializeField] private UnityEvent<int> onDayStarted;
-    [SerializeField] private GameObject dayCutscene;
-    [SerializeField] private GameObject infoTextBar;
-    [SerializeField] private TMP_Text dayText;
-    [SerializeField] private TimeController timeController;
-    
-    private int _day = 0;
-    private void Start()
     {
-        NewDay();
+        
+        [SerializeField] private UnityEvent<int> onDayStarted;
+        [SerializeField] private GameObject dayCutscene;
+        [SerializeField] private GameObject infoTextBar;
+        [SerializeField] private TMP_Text dayText;
+        [SerializeField] private TimeController timeController;
+        
+        private int _day = 0;
+        private void Start()
+        {
+            NewDay();
+        }
+        
+        [Button]
+        public void NewDay()
+        {
+            
+            StartCoroutine(StartNewDay());
+        }  
+        
+        private IEnumerator StartNewDay()
+        {
+            _day++;
+            onDayStarted.Invoke(_day);
+            yield return new WaitForFixedUpdate();
+            dayText.text = $"Day {_day}";
+            dayCutscene.SetActive(true);
+            
+            yield return new WaitForSeconds(3f);
+            
+            dayCutscene.SetActive(false);
+            
+            timeController.StartTimer();
+        }
     }
-    
-    [Button]
-    public void NewDay()
-    {
-        
-        StartCoroutine(StartNewDay());
-    }  
-    
-    private IEnumerator StartNewDay()
-    {
-        _day++;
-        onDayStarted.Invoke(_day);
-        yield return new WaitForFixedUpdate();
-        dayText.text = $"Day {_day}";
-        dayCutscene.SetActive(true);
-        
-        yield return new WaitForSeconds(3f);
-        
-        dayCutscene.SetActive(false);
-        
-        timeController.StartTimer();
-    }
-}
